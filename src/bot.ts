a55db632f16db803b281fa24b7b57f0b4c9ba066
wrac4242 <wrac4242@gmail.com>
2022-03-06 12:06:20 +0000

    add eslint and script setup

--- src/bot.ts
+++ src/bot.ts
@@ -1,12 +1,12 @@
-import { Client } from "discord.js"
-import { readdirSync } from "fs"
-import { token } from "./config.json"
+import { Client } from 'discord.js'
+// import { readdirSync } from "fs"
+import { token } from './config.json'
 
-console.log("\n---\n")
+console.log('\n---\n')
 
 const client = new Client({ intents: 0 })
 
-client.once("ready", () => {
+client.once('ready', () => {
     console.log(`Logged in as ${client.user?.tag}`)
 })
 
